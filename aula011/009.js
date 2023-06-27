81d0031f3f6b3f714e5aa051de5a02d4971d0a13
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-06-27 17:51:37 -0300

    aula 06 até 012

---
+++ aula011/009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+
+if (pais == 'BRASIL') {
+    console.log('Voce é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 100) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO.`)
+} else {
+    console.log(`Diriga com cuidado.`)
+}
