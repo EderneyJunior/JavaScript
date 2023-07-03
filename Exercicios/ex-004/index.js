d5ec428a4e9f39735ea46349ffe3138fb8427815
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-03 12:08:48 -0300

    ex-004 Ajustado

--- Exercicios/ex-004/index.js
+++ Exercicios/ex-004/index.js
@@ -1,17 +1,17 @@
 function tabuada() {
-    var num = document.getElementById('txtn')
-    var tab = document.getElementById('seltab')
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-    if (num.value.length === 0) {
-        window.alert('[ERRO]. Digite um numero!')
-    } else {
-        var n = Number(num.value)
+    if (num.value.length == 0) {
+        alert('[ERRO]. por favor digite um número!')
+    } else{
+        let n = Number(num.value)
         tab.innerHTML = ''
-        
-        for (var c = 1; c <= 10; c++) {
-            var item = document.createElement('option')
 
-            item.text = `${n} X ${c} = ${n * c}`
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+
+            item.text = `${n} X ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
         }
