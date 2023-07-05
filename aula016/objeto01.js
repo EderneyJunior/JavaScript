5768bbc9a2fe46313f95e32ed026379997f52c67
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-05 13:55:28 -0300

    Arrumei o repositório JS

--- aula016/objeto01.js
+++
@@ -1,7 +0,0 @@
-let amigo = {nome: 'Ederney', Idade: 20, altura: 1.70, sexo: 'M', peso: 64, engordar(p=0){
-    console.log('Engordou')
-    this.peso += p
-}}
-
-amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
